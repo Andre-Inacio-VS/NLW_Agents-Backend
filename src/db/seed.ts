09c91ad7e65ebb9769eed363c8949934f0f22beb
Andre-Inacio-VS <andreinaciovs@gmail.com>
2025-07-08 20:20:30 -0300

    fix: corrigindo gerador de dados fake para não gerar salas com data de criação muito antiga e nem acima da data atual

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,10 +7,14 @@ await reset(db, schema);
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
+        createdAt: f.date({
+          minDate: new Date("2025-01-01"),
+          maxDate: new Date(),
+        }),
       },
       with: {
         questions: 5,
